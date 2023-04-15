be3b0564041054172a8239368c0b95a27f9c3272
lili.farkas <lili.farkas14@gmail.com>
2023-04-15 22:54:29 +0200

    Create Header base

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,23 @@
+
+import './Header.css';
+
+function Header(){
+    
+    
+    return (
+        <div>
+            <header>
+                <div className="container-fluid p-5 text-center bg-light">
+                    <div className="row">
+                        <div className="col-lg-6 mx-auto">
+                            <h1 className="mb-3">Alternatív Prevenciós és Rehabilitációs Központ</h1>
+                            <a className="btn btn-primary" href="#" role="button">Contact us</a>
+                        </div>
+                    </div>
+                </div>
+            </header>
+        </div>
+    )
+}
+
+export default Header;
