baa6e5297d6ebee9b0373b136861eab7d3566bd6
lili.farkas <lili.farkas14@gmail.com>
2023-04-15 22:57:58 +0200

    Create the About section base

---
+++ frontend/src/components/About/About.jsx
@@ -0,0 +1,21 @@
+import './About.css'
+import {useState} from "react";
+
+
+function MainPage(){
+    
+    
+    return(
+        <div className="main-page">
+            <div className="main-page-header">
+                <h1>About Us</h1>
+            </div>
+            <div className="main-page-content">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in justo in dolor auctor lacinia sit amet vel ipsum. Vivamus tincidunt turpis nec dui aliquet, ut bibendum mi eleifend. Praesent efficitur, libero sed laoreet luctus, mauris ipsum vehicula velit, eget sollicitudin nisi quam in odio. Integer et tellus eget neque vestibulum dignissim eget sit amet tellus. Sed semper eleifend odio, nec ullamcorper nulla ultrices eget. Proin volutpat urna ex, vitae rhoncus nibh auctor vel.</p>
+                <p>Nullam bibendum ornare quam id tincidunt. Nulla auctor, elit a faucibus aliquam, neque orci vehicula enim, at maximus odio arcu sit amet turpis. Sed scelerisque ante sed enim aliquam, id bibendum nibh pharetra. Ut rutrum venenatis lorem, vitae venenatis tellus faucibus quis. Nulla posuere ipsum sapien, a bibendum dolor viverra in. Vivamus pellentesque suscipit dapibus. Nam ac ullamcorper magna, quis interdum odio. Integer quis mi a velit placerat bibendum. In hac habitasse platea dictumst. Suspendisse sodales facilisis commodo.</p>
+            </div>
+        </div>
+    )
+}
+
+export default MainPage;
