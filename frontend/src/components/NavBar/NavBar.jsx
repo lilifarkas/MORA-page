795ddca8ea754b452135b5a7dcf2e9f77b001e9d
lili.farkas <lili.farkas14@gmail.com>
2023-05-12 14:25:16 +0200

    Create the NavBar design

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,42 +1,91 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './NavBar.css';
 import pic from "./1660149889759865_page-0001-removebg-preview.png"
+import { Link } from "react-scroll";
 
 
 function NavBar(){
-
-
+    
     return(
-        <div>
-            <nav className="navbar navbar-expand-lg custom-bg-color">
+        <header id="home">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <a className="navbar-brand" href="#">
-                        <img src={pic} alt="Logo" />
+                        <img
+                            src={pic}
+                            alt="Logo"
+                        />
                     </a>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
-                            data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                            aria-label="Toggle navigation">
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
                     </button>
-                    <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
-                        <ul className="navbar-nav">
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
                             <li className="nav-item">
-                                <a className="nav-link " aria-current="page" href="#">Home</a>
+                                <Link
+                                    activeClass="active"
+                                    to="home"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={200}
+                                    className="nav-link"
+                                >
+                                    Home
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " href="#">About</a>
+                                <Link
+                                    activeClass="active"
+                                    to="about"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={200}
+                                    className="nav-link"
+                                >
+                                    About
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " href="#">Book apointment</a>
+                                <Link
+                                    activeClass="active"
+                                    to="appointment"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={200}
+                                    className="nav-link"
+                                >
+                                    Book appointment
+                                </Link>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link " href="#">Contact</a>
+                                <Link
+                                    activeClass="active"
+                                    to="contact"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={200}
+                                    className="nav-link"
+                                >
+                                    Contact
+                                </Link>
                             </li>
                         </ul>
                     </div>
                 </div>
             </nav>
-        </div>
+        </header>
     )
 }
 
