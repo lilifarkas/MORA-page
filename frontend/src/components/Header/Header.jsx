3c3b0be2c2f7fd04e059a4b427e582fa805d3a78
lili.farkas <lili.farkas14@gmail.com>
2023-05-12 14:25:37 +0200

    Create the Header design

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,17 +1,32 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import './Header.css';
+import { Link } from "react-scroll";
 
 function Header(){
     
     
     return (
+        
         <div>
-            <header>
-                <div className="container-fluid p-5 text-center bg-light">
+            <header className="main">
+                <div className="container-fluid p-4 text-center bg-light">
                     <div className="row">
-                        <div className="col-lg-6 mx-auto">
-                            <h1 className="mb-3">Alternatív Prevenciós és Rehabilitációs Központ</h1>
-                            <a className="btn btn-primary" href="#" role="button">Contact us</a>
+                        <div className="">
+                            <h1 className="">Prevention and Rehabilitation Center</h1>
+                            <a className="btn btn-primary" href="#" role="button">
+                                <Link
+                                    activeClass="active"
+                                    to="appointment"
+                                    spy={true}
+                                    smooth={true}
+                                    offset={-70}
+                                    duration={200}
+                                    className="nav-link ctaButton"
+                                >
+                                    Book appointment
+                                </Link>
+                            </a>
                         </div>
                     </div>
                 </div>
