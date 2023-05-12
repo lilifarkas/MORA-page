195ca4141d9f0fe86437369ebe472806ff24b5b1
lili.farkas <lili.farkas14@gmail.com>
2023-05-12 14:20:51 +0200

    Create the appointment booking system base in frontend

--- frontend/src/components/BookApointment/Book.jsx
+++ frontend/src/components/BookApointment/Book.jsx
@@ -1,22 +1,103 @@
 import { Calendar } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 import {useState} from "react";
+import './Book.css'
+import bgImg from "./Névtelen terv (23).png";
 
 
 
 function Book(){
 
     const [date, setDate] = useState(new Date());
+    const greyDays = [1, 2, 0]; // Monday, Tuesday, Sunday
+    const greenDays = [3, 4, 5, 6]; // Wednesday, Thursday, Friday, Saturday
+    const [clickedGreyDay, setClickedGreyDay] = useState(false);
+    const [selectedDate, setSelectedDate] = useState(null);
 
+    function getTileClassName({ date, view }) {
+        const dayOfWeek = date.getDay();
+        if (greyDays.includes(dayOfWeek)) {
+            return 'grey-day';
+        } else if (greenDays.includes(dayOfWeek)) {
+            return 'green-day';
+            
+        }
+    }
+    
     function handleDateChange(date) {
         setDate(date);
     }
 
+    function handleDayClick(date) {
+        if (greyDays.includes(date.getDay())) {
+            console.log('Clicked on a grey day');
+            setClickedGreyDay(true);
+            setSelectedDate(null);
+        } else {
+            console.log('Clicked on a green day');
+            setClickedGreyDay(false);
+            setSelectedDate(date);
+        }
+    }
+
     return(
-        <div>
-            <Calendar value={date} onChange={handleDateChange} />
+        <div id="appointment">
+            <div className="hero">
+                <img src={bgImg} alt="doctor" />
+                <div className="hero-overlay"></div>
+                <div className="hero-text">
+                    <h1>Book appointment</h1>
+                </div>
+            </div>
+            <div className="d-flex justify-content-center">
+                <Calendar
+                    value={selectedDate || new Date()}
+                    onChange={handleDateChange}
+                    onClickDay={handleDayClick}
+                    tileClassName={getTileClassName}
+                />
+            </div>
+            {clickedGreyDay && (
+                <div className="message">
+                    <h2>This day is unavailable!</h2>
+                </div>
+            )}
+            <div className="hours-container">
+                {selectedDate && greenDays.includes(selectedDate.getDay()) && (
+                    <AvailableHours date={selectedDate} />
+                )}
+            </div>
         </div>
     )
+}
+
+function AvailableHours({ date }) {
+    const hours = [
+        "08:00",
+        "09:00",
+        "10:00",
+        "11:00",
+        "12:00",
+        "13:00",
+        "14:00",
+        "15:00",
+        "16:00",
+        "17:00",
+        "18:00",
+        "19:00",
+        "20:00",
+    ];
+
+    return (
+        <div>
+            <h2>Available hours for {date.toDateString()}</h2>
+            <ul>
+                {hours.map((hour) => (
+                    <li key={hour}>{hour}</li>
+                ))}
+            </ul>
+        </div>
+    );
 }
 
 
