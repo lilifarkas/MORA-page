8e7038b4ad4163e5834e471085c2e8f76dff0104
lili.farkas <lili.farkas14@gmail.com>
2023-05-12 14:19:26 +0200

    Design the about section

--- frontend/src/components/About/About.jsx
+++ frontend/src/components/About/About.jsx
@@ -1,18 +1,68 @@
 import './About.css'
-import {useState} from "react";
-
+import bgImg from './Névtelen terv (22).png';
+import doctorImg from './Névtelen terv (17).png';
+import missionImg from './Névtelen terv (24).png';
 
 function MainPage(){
     
     
     return(
-        <div className="main-page">
-            <div className="main-page-header">
-                <h1>About Us</h1>
+        <div className="about-page" id="about">
+            <div className="hero">
+                <img src={bgImg} alt="doctor" />
+                <div className="hero-overlay"></div>
+                <div className="hero-text">
+                    <h1>About Us</h1>
+                </div>
             </div>
-            <div className="main-page-content">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis in justo in dolor auctor lacinia sit amet vel ipsum. Vivamus tincidunt turpis nec dui aliquet, ut bibendum mi eleifend. Praesent efficitur, libero sed laoreet luctus, mauris ipsum vehicula velit, eget sollicitudin nisi quam in odio. Integer et tellus eget neque vestibulum dignissim eget sit amet tellus. Sed semper eleifend odio, nec ullamcorper nulla ultrices eget. Proin volutpat urna ex, vitae rhoncus nibh auctor vel.</p>
-                <p>Nullam bibendum ornare quam id tincidunt. Nulla auctor, elit a faucibus aliquam, neque orci vehicula enim, at maximus odio arcu sit amet turpis. Sed scelerisque ante sed enim aliquam, id bibendum nibh pharetra. Ut rutrum venenatis lorem, vitae venenatis tellus faucibus quis. Nulla posuere ipsum sapien, a bibendum dolor viverra in. Vivamus pellentesque suscipit dapibus. Nam ac ullamcorper magna, quis interdum odio. Integer quis mi a velit placerat bibendum. In hac habitasse platea dictumst. Suspendisse sodales facilisis commodo.</p>
+            <div className="main-content">
+                <div className="container">
+                    <div className="row align-items-center">
+                        <div className="col-md-6 text-center text-container">
+                            <h2>Welcome to Our Clinic</h2>
+                            <p>At Our Clinic, we are committed to providing the highest quality medical care to our patients. Our team of highly trained medical professionals will work with you to create a personalized treatment plan that meets your specific needs and goals.</p>
+                            <p>Whether you are dealing with a chronic condition or simply need a routine check-up, we are here to help. Contact us today to schedule an appointment and experience the difference of personalized medical care.</p>
+                        </div>
+                        <div className="col-md-4">
+                            <img src={doctorImg} alt="two doctors smiling" className="doctorsPic"/>
+                        </div>
+                    </div>
+
+                    <div className="row align-items-center">
+                        <div className="col-md-6">
+                            <img src={missionImg} alt="doctor and a patient" className="doctorsPic"/>
+                        </div>
+                        <div className="col-md-6 text-center ">
+                            <h2 className="text-title">Our Mission</h2>
+                            <p className="text-container2 text-center">Our mission is to provide compassionate, patient-centered medical care to the communities we serve. We believe that every patient deserves to be treated with respect, dignity, and kindness.</p>
+                            <p className="text-container2 text-center">We are committed to staying at the forefront of medical research and technology, so that we can continue to offer our patients the latest and most effective treatments available.</p>
+                        </div>
+                    </div>
+                    
+                    <div className="row mt-5 mb-5">
+                        <div className="col-md-4">
+                            <div className="service">
+                                <i className="fas fa-user-md"></i>
+                                <h3>Experienced Staff</h3>
+                                <p>Our team of medical professionals has years of experience and is dedicated to providing the highest quality care to our patients.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="service">
+                                <i className="fas fa-calendar-alt"></i>
+                                <h3>Convenient Appointments</h3>
+                                <p>We offer flexible scheduling options, so that you can get the care you need when it is most convenient for you.</p>
+                            </div>
+                        </div>
+                        <div className="col-md-4">
+                            <div className="service">
+                                <i className="fas fa-heartbeat"></i>
+                                <h3>Compassionate Care</h3>
+                                <p>We treat every patient with the respect, dignity, and kindness they deserve. You can trust us to provide the care you need.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
