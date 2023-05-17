214581a91ae8aab24559673e346258edf6112b16
lili.farkas <lili.farkas14@gmail.com>
2023-05-17 19:16:43 +0200

    Create UserController

--- backend/backend/Controllers/UserController.cs
+++ backend/backend/Controllers/UserController.cs
@@ -1,3 +1,5 @@
+using backend.Models.Entities;
+using backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -5,5 +7,34 @@ namespace backend.Controllers;
 [ApiController, Route("/user")]
 public class UserController: ControllerBase
 {
+    private readonly UserService _service;
+   
+    public UserController(UserService service)
+    {
+        _service = service;
+    }
     
+    [HttpGet("/users/{id}")]
+    public async Task<User> GetUserById(long id)
+    {
+        return await _service.Get(id);
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<User>> GetAllUser()
+    {
+        return await _service.GetAll();
+    }
+    
+    [HttpPut("/users/update/{id}")]
+    public async Task UpdateUser(long id, [FromBody] User user)
+    {
+        await _service.Update(user);
+    }
+    
+    [HttpDelete("/users/delete/{id}")]
+    public async Task DeleteUser(long id)
+    {
+        await _service.Delete(id);
+    }
 }
