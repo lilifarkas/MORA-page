61eda7bc4ca8b31b0dca21344e271f9326c11b89
lili.farkas <lili.farkas14@gmail.com>
2023-05-17 19:45:25 +0200

    Create password hasher interface

---
+++ backend/backend/Services/PasswordHasher/PasswordHasher.cs
@@ -0,0 +1,14 @@
+namespace backend.Services.PasswordHasher;
+
+public class PasswordHasher: IPasswordHasher
+{
+    public string HashPassword(string password)
+    {
+        throw new NotImplementedException();
+    }
+
+    public bool VerifyPassword(string password, string passwordHash)
+    {
+        throw new NotImplementedException();
+    }
+}
