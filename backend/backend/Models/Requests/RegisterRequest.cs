ab2450a5597912877ed1bc230b40604aeabafcb3
lili.farkas <lili.farkas14@gmail.com>
2023-05-18 19:55:37 +0200

    Add phone number property to the RegisterRequest

--- backend/backend/Models/Requests/RegisterRequest.cs
+++ backend/backend/Models/Requests/RegisterRequest.cs
@@ -11,7 +11,9 @@ public class RegisterRequest
     [EmailAddress]
     public string Email { get; set; }
     [Required]
-    public string password { get; set; }
+    public string PhoneNumber { get; set; }
+    [Required]
+    public string Password { get; set; }
     [Required]
     public string ConfirmPassword { get; set; }
 }
