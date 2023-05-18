4e87372858eea9f72750fdfcc351a7a5c51b7f4b
lili.farkas <lili.farkas14@gmail.com>
2023-05-18 19:53:47 +0200

    Implement the PasswordHasher methods

--- backend/backend/Services/PasswordHasher/PasswordHasher.cs
+++ backend/backend/Services/PasswordHasher/PasswordHasher.cs
@@ -4,11 +4,11 @@ public class PasswordHasher: IPasswordHasher
 {
     public string HashPassword(string password)
     {
-        throw new NotImplementedException();
+        return BCrypt.Net.BCrypt.HashPassword(password);
     }
 
     public bool VerifyPassword(string password, string passwordHash)
     {
-        throw new NotImplementedException();
+        return BCrypt.Net.BCrypt.Verify(password, passwordHash);
     }
 }
