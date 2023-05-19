f13ed87a45d618378230965c67d2472e47c1de89
lili.farkas <lili.farkas14@gmail.com>
2023-05-19 17:01:05 +0200

    Create main endpoints for the DateController

--- backend/backend/Controllers/DateController.cs
+++ backend/backend/Controllers/DateController.cs
@@ -1,10 +1,11 @@
 using backend.Models.Entities;
+using backend.Models.Requests;
 using backend.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
 
-[ApiController, Route("/date")]
+[ApiController, Route("/dates")]
 public class DateController: ControllerBase
 {
     private readonly DateService _service;
@@ -17,7 +18,33 @@ public class DateController: ControllerBase
     [HttpGet("{userID}/allDates")]
     public async Task<IEnumerable<BookedDate>> GetUserDates(long userID)
     {
-        // return await _service.;
-        throw new NotImplementedException();
+        return await _service.GetUserDates(userID);
+
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<BookedDate>> GetAllDates()
+    {
+        return await _service.GetAll();
+    }
+
+    [HttpPost("registerDate")]
+    public async Task<IActionResult> Register([FromBody] RegisterDateRequest registerDateRequest)
+    {
+        var registrationDate = new BookedDate()
+        {
+            User = registerDateRequest.User,
+            Date = registerDateRequest.Date,
+            BookedTime = registerDateRequest.BookedTime
+        };
+
+        var response = await _service.Add(registrationDate);
+        return Ok(response);
+    }
+    
+    [HttpDelete("/dates/delete/{id}")]
+    public async Task DeleteBookedDate(long id)
+    {
+        await _service.Delete(id);
     }
 }
