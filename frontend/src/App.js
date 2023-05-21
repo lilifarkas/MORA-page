f38adf1bce036219dadeec293e173fe3b961bb0e
lili.farkas <lili.farkas14@gmail.com>
2023-05-21 22:12:47 +0200

    Implement Routing through the components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,18 +5,24 @@ import Header from "./components/Header/Header"
 import Book from "./components/BookApointment/Book";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
+import LogIn from "./components/LogIn/LogIn"
+import Register from "./components/Register/Register"
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-      <div className='App'>
-          < NavBar />
-          < Header />
-          < About />
-          < Book />
-          < Contact />
-          < Footer />
-      </div>
-  );
+
+    return (
+        <Router>
+            <div className="App">
+                <Routes>
+                    <Route path="/" element={[<NavBar/>, <Header />, <About/>, <Book/>, <Contact/>, <Footer/>]} />
+                    <Route path="/login" element={<LogIn />} />
+                    <Route path="/register" element={<Register />} />
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
