b12039a9065c9fcd9d4f3691297bfc577634acf2
lili.farkas <lili.farkas14@gmail.com>
2023-05-23 13:39:58 +0200

    Fix User entity

--- backend/backend/Models/Entities/User.cs
+++ backend/backend/Models/Entities/User.cs
@@ -7,8 +7,9 @@ public class User
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public long ID { get; set; }
     public string Name { get; set; }
-    public string Password { get; set; }
+    public string Role { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
-    public virtual ICollection<BookedDate>? BookedDatess { get; set; } = new List<BookedDate>();
+    public string Password { get; set; }
+    public virtual ICollection<BookedDate>? BookedDates { get; set; } = new List<BookedDate>();
 }
