793b0d9d0e0462c9fd349b94d29083efd1d79958
lili.farkas <lili.farkas14@gmail.com>
2023-05-25 21:20:33 +0200

    Create LogIn component

--- frontend/src/components/LogIn/LogIn.jsx
+++ frontend/src/components/LogIn/LogIn.jsx
@@ -1,14 +1,93 @@
+import React, {useEffect, useState} from 'react';
+import './LogIn.css';
+import bgImg from "./Névtelen terv (25).png";
+import {NavLink} from "react-router-dom";
+import { FiArrowLeft } from 'react-icons/fi';
 
+function LogIn() {
+    const[LogInForm, setLogInForm] = useState({
+        "username": "",
+        "password": ""
+    })
 
-function LogIn(){
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
+        const response = await fetch('https://localhost:7230/login',{
+            method: 'POST',
+            headers: {
+                // 'Authorization' : `Bearer ${localStorage.getItem("token")}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(LogInForm)
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            alert(errorResponse)
+            console.log(errorResponse);
+            return
+        }
+
+        setLogInForm({
+            "username": '',
+            "password": ''
+        });
+
+        if(response.ok) alert("User logged in")
+        setTimeout(() => {
+
+        }, 1000);
+    };
 
     return (
-        <div className="">
-            <p>LOGIN</p>
+        <div className="main1" id="contact">
+            <div className="main2">
+                <img src={bgImg} alt="doctor" />
+                <div className="hero-overlay"></div>
+                <div className="hero-text">
+                    <NavLink
+                        to="/"
+                        className="back"
+                    >
+                        <FiArrowLeft className="back-icon" />
+                        Back
+                    </NavLink>
+                    <h1>Log in</h1>
+                    <form onSubmit={handleSubmit}>
+                        <div className="form-group">
+                            <label htmlFor="nameInput">Name</label>
+                            <input
+                                type="text"
+                                className="form-control"
+                                id="name"
+                                placeholder="Enter your name"
+                                style={{textAlign: 'center'}}
+                                value={LogInForm.username}
+                                onChange={(e) => setLogInForm({...LogInForm, username: e.target.value})}
+                            />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="emailInput">Password</label>
+                            <input
+                                type="password"
+                                className="form-control"
+                                id="passwordInput"
+                                placeholder="Enter the password"
+                                style={{textAlign: 'center'}}
+                                value={LogInForm.password}
+                                onChange={(e) => setLogInForm({...LogInForm, password: e.target.value})}
+                            />
+                        </div>
+                        <button type="submit" className="btn btn-primary">
+                            Log in
+                        </button>
+                    </form>
+                </div>
+            </div>
+
         </div>
     );
 }
-
 
 export default LogIn;
