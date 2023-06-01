d57f445396b4f9d1a077c535fe1774c5a6014cc2
lili.farkas <lili.farkas14@gmail.com>
2023-06-01 15:53:08 +0200

    Implement the Profile component with a custom hook

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -1,23 +1,54 @@
 import './Profile.css'
-import bgImg from "../Register/Névtelen terv (25).png";
-import React from "react";
-
+import bgImg from "../../images/Névtelen terv (28).png";
+import React, {useEffect, useState} from "react";
+import {NavLink, useNavigate} from "react-router-dom";
+import { FiArrowLeft } from 'react-icons/fi';
+import useFetchUser from '../../hooks/useFetchUser';
+import useFetch from '../../hooks/useFetch';
 
 function Profile(){
+    const user = useFetchUser();
+    const navigate = useNavigate();
     
+    const handleEditProfile = async (e) => {
+        e.preventDefault();
+        
+        navigate("/edit");
+    };
+
 
     return (
-        <div className="main2">
-            <img src={bgImg} alt="doctor" />
-            <div className="hero-overlay"></div>
-            <div className="hero-text">
-                <div>
-
-                </div>
-                <div className="buttons d-flex flex-row gap-3">
-                    <button className="btn btn-primary">EDIT</button>
-                    <button className="btn btn-primary">DELETE</button>
-                    <button className="btn btn-primary">BOOK APPPOINTMENT</button>
+        <div className="main3">
+            <div className="main2">
+                <img src={bgImg} alt="doctor" />
+                <div className="hero-overlay"></div>
+                <div className="hero-text">
+                    <div className="back-button">
+                        <NavLink
+                            to="/"
+                            className="back"
+                        >
+                            <FiArrowLeft className="back-icon" />
+                            Back
+                        </NavLink>
+                    </div>
+                    <div className="d-flex flex-row main3 justify-content-center align-items-center">
+                        <div>
+                            {user && <>
+                                <h1>Name: {user.name}</h1>
+                                <h1>Email: {user.email}</h1>
+                                <h1>Phone Number: {user.phone}</h1>
+                                <h1>Booked dates: {user.bookedDates}</h1>
+                            </>}
+                        </div>
+                        <div className="buttons d-flex flex-column gap-3">
+                            <button className="btn btn-primary profile-buttons" onClick={handleEditProfile}>EDIT PROFILE</button>
+                            <button className="btn btn-primary profile-buttons">DELETE PROFILE</button>
+                            <button className="btn btn-primary profile-buttons">CHANGE PASSWORD</button>
+                            <button className="btn btn-primary profile-buttons">BOOK APPPOINTMENT</button>
+                        </div>
+                    </div>
+                    
                 </div>
             </div>
         </div>
