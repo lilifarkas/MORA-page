241e99a252fa07847485eb0494d25f76b223451f
lili.farkas <lili.farkas14@gmail.com>
2023-06-01 16:50:06 +0200

    Implement the delete user functionality with react-modal

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -4,17 +4,36 @@ import React, {useEffect, useState} from "react";
 import {NavLink, useNavigate} from "react-router-dom";
 import { FiArrowLeft } from 'react-icons/fi';
 import useFetchUser from '../../hooks/useFetchUser';
-import useFetch from '../../hooks/useFetch';
+import Modal from 'react-modal';
 
 function Profile(){
     const user = useFetchUser();
     const navigate = useNavigate();
+    const [showModal, setShowModal] = useState(false);
     
     const handleEditProfile = async (e) => {
         e.preventDefault();
-        
         navigate("/edit");
     };
+
+    const deleteUser = async (e) => {
+        e.preventDefault();
+        await fetch(`https://localhost:7230/users/delete/${user.id}`, {
+            method: "DELETE",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            credentials: 'include'
+
+        });
+        
+        setShowModal(false);
+        navigate("/");
+    }
+
+    const handleCancel = () => {
+        setShowModal(false);
+    }
 
 
     return (
@@ -28,10 +47,11 @@ function Profile(){
                             to="/"
                             className="back"
                         >
-                            <FiArrowLeft className="back-icon" />
+                            <FiArrowLeft className="back-icon" /> 
                             Back
                         </NavLink>
                     </div>
+                    <h1>Profile</h1>
                     <div className="d-flex flex-row main3 justify-content-center align-items-center">
                         <div>
                             {user && <>
@@ -43,11 +63,24 @@ function Profile(){
                         </div>
                         <div className="buttons d-flex flex-column gap-3">
                             <button className="btn btn-primary profile-buttons" onClick={handleEditProfile}>EDIT PROFILE</button>
-                            <button className="btn btn-primary profile-buttons">DELETE PROFILE</button>
+                            <button className="btn btn-primary profile-buttons" onClick={() => setShowModal(true)}>DELETE PROFILE</button>
                             <button className="btn btn-primary profile-buttons">CHANGE PASSWORD</button>
                             <button className="btn btn-primary profile-buttons">BOOK APPPOINTMENT</button>
                         </div>
                     </div>
+
+                    <Modal
+                        isOpen={showModal}
+                        onRequestClose={() => setShowModal(false)}
+                        contentLabel="Delete Profile Modal"
+                        className="modalDelete"
+                    >
+                        <h2 className="titles">Are you sure you want to delete your profile?</h2>
+                        <div className="d-flex flex-row gap-5 mt-3">
+                            <button className="btn btn-primary" onClick={deleteUser}>YES</button>
+                            <button className="btn btn-primary" onClick={handleCancel}>NO</button>
+                        </div>
+                    </Modal>
                     
                 </div>
             </div>
