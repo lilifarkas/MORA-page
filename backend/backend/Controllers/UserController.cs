c3b79be07079889e18bd56b3379c0dc640c658e5
lili.farkas <lili.farkas14@gmail.com>
2023-06-05 18:38:20 +0200

    Add ChangePassword method to the controller

--- backend/backend/Controllers/UserController.cs
+++ backend/backend/Controllers/UserController.cs
@@ -1,5 +1,8 @@
 using backend.Models.Entities;
+using backend.Models.Requests;
 using backend.Services;
+using backend.Services.PasswordHasher;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace backend.Controllers;
@@ -8,10 +11,12 @@ namespace backend.Controllers;
 public class UserController: ControllerBase
 {
     private readonly IUserService _service;
+    private readonly IPasswordHasher _passwordHasher;
    
-    public UserController(IUserService service)
+    public UserController(IUserService service, IPasswordHasher passwordHasher)
     {
         _service = service;
+        _passwordHasher = passwordHasher;
     }
     
     [HttpGet("/users/{id}")]
@@ -30,6 +35,25 @@ public class UserController: ControllerBase
     public async Task UpdateUser(long id, [FromBody] User user)
     {
         await _service.Update(user, id);
+    }
+    
+    [HttpPut("/change-pass/{id}")]
+    public async Task<IActionResult> ChangePassword(long id, [FromBody] ChangePasswordRequest changePasswordRequest)
+    {
+        var user = await _service.GetById(id);
+        if (user == null)
+        {
+            return Unauthorized();
+        }
+
+        var isCorrectPassword = _passwordHasher.VerifyPassword(changePasswordRequest.Password, user.Password);
+        if (!isCorrectPassword)
+        {
+            return Unauthorized();
+        }
+
+        await _service.ChangePassword(id, changePasswordRequest);
+        return Ok();
     }
     
     [HttpDelete("/users/delete/{id}")]
