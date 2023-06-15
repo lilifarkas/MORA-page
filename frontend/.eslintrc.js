a59b1cacd9e08dfb9c906d302f0a3968b3ae5c71
lili.farkas <lili.farkas14@gmail.com>
2023-06-15 19:38:41 +0200

    Add a rule to the eslint file

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -17,6 +17,7 @@ module.exports = {
         "react/react-in-jsx-scope": "off",
         "react/prop-types": "off",
         "no-unused-vars": "off",
-        "no-undef": "off"
+        "no-undef": "off",
+        'react/jsx-key': 'off'
     }
 }
