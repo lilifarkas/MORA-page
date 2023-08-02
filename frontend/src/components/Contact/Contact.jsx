e07556d4250940b6997f28d8d20e50c9f180d1a7
lili.farkas <lili.farkas14@gmail.com>
2023-08-02 23:30:12 +0200

    Add a loading spinner to the contact section

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, {useEffect, useState} from 'react';
 import './Contact.css';
 import bgImg from "../../images/Névtelen terv (20).png";
 import emailjs from 'emailjs-com';
+import { BeatLoader } from "react-spinners";
 
 function Contact() {
     const [name, setName] = useState('');
@@ -9,6 +10,7 @@ function Contact() {
     const [message, setMessage] = useState('');
     const [error, setError] = useState('');
     const [success, setSuccess] = useState('');
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
         let timer;
@@ -27,11 +29,10 @@ function Contact() {
         } else if (!/\S+@\S+\.\S+/.test(email)) {
             setError('Please enter a valid email address');
         } else {
-            console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
-            // Send email
+            setLoading(true);
             emailjs.send(
                 'service_m8bql7c',
-                'template_gf5b8yl',
+                'template_b0bmwv3',
                 { from_name: name, from_email: email, message: message },
                 '_OPM3OGf4upRH17EZ'
             )
@@ -42,9 +43,11 @@ function Contact() {
                     setMessage('');
                     setError('');
                     setSuccess('Thank you for your message! We will contact you as soon as possible!');
+                    setLoading(false);
                 }, function(error) {
                     console.log('FAILED...', error);
                     setError('Failed to send email. Please try again later.');
+                    setLoading(false);
                 });
         }
     }
@@ -97,7 +100,14 @@ function Contact() {
                         </div>
                         {success && <div className="success" style={{color: '#ffffff'}}>{success}</div>}
                         <button type="submit" className="btn btn-primary" disabled={!isFormValid}>
-                            Submit
+                            {loading ? ( // Show the loading spinner when loading is true
+                                <BeatLoader
+                                    color={"#fff"}
+                                    size={8}
+                                />
+                            ) : (
+                                "Send message"
+                            )}
                         </button>
                     </form>
                 </div>
