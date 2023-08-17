e48d161900b16df9744e75f2e84421a632b66881
lili.farkas <lili.farkas14@gmail.com>
2023-08-17 23:19:38 +0200

    Implement the handleSubmit function

--- frontend/src/components/AdminsPage/AdminRegistration.jsx
+++ frontend/src/components/AdminsPage/AdminRegistration.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import Modal from "react-modal";
 import { FiX } from "react-icons/fi";
 import './AdminRegistration.css'
+import URL from "../../Constants/ConstantUrl";
+import {useNavigate} from "react-router-dom";
 
 const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
     const [formData, setFormData] = useState({
@@ -11,19 +13,47 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
         "password": "",
         "confirmPassword": ""
     });
+    const navigate = useNavigate();
 
-    const handleInputChange = (e) => {
-        const { name, value } = e.target;
-        setFormData((prevData) => ({
-            ...prevData,
-            [name]: value,
-        }));
-    };
-
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        const response = await fetch(`${URL}register-admin`,{
+            method: 'POST',
+            headers: {
+                // 'Authorization' : `Bearer ${localStorage.getItem("token")}`,
+                'Content-Type': 'application/json'
+            },
+            credentials: 'include',
+            body: JSON.stringify(formData)
+        });
+
+        if (!response.ok) {
+            const errorResponse = await response.json();
+            // const errorMessage = errorResponse.errorMessages[0];
+            alert(errorResponse)
+            console.log(errorResponse);
+            return
+        }
+
+        setFormData({
+            "name": '',
+            "email": '',
+            "phoneNumber": '',
+            "password": '',
+            "confirmPassword": ''
+        });
+
+        if(response.ok) {
+            alert("Admin Successfully Registered")
+            navigate('/adminsPage');
+        }
+        setTimeout(() => {
+
+        }, 1000);
         
         onRequestClose();
+        window.location.reload();
     };
 
     return (
@@ -32,9 +62,10 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
             onRequestClose={onRequestClose}
             className="modal"
             overlayClassName="modal-overlay"
+            appElement={document.getElementById('root') || undefined}
         >
             <div className="admin-main-container">
-                <button type="button" onClick={onRequestClose}>
+                <button type="button" onClick={onRequestClose} className="align-self-start">
                     <FiX />
                 </button>
                 <h2 className="register-admin-title">Register New Admin</h2>
@@ -45,7 +76,7 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
                             type="text"
                             className="form-control"
                             id="name"
-                            placeholder="Enter your name"
+                            placeholder="Enter name"
                             style={{textAlign: 'center'}}
                             value={formData.name}
                             onChange={(e) => setFormData({...formData, name: e.target.value})}
@@ -57,7 +88,7 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
                             type="email"
                             className="form-control"
                             id="email"
-                            placeholder="Enter your email"
+                            placeholder="Enter email"
                             style={{textAlign: 'center'}}
                             value={formData.email}
                             onChange={(e) => setFormData({...formData, email: e.target.value})}
@@ -69,7 +100,7 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
                             type="text"
                             className="form-control"
                             id="phoneNumber"
-                            placeholder="Enter your phone number"
+                            placeholder="Enter phone number"
                             style={{textAlign: 'center'}}
                             value={formData.phoneNumber}
                             onChange={(e) => setFormData({...formData, phoneNumber: e.target.value})}
@@ -81,7 +112,7 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
                             type="password"
                             className="form-control"
                             id="passwordInput"
-                            placeholder="Enter the password"
+                            placeholder="Enter password"
                             style={{textAlign: 'center'}}
                             value={formData.password}
                             onChange={(e) => setFormData({...formData, password: e.target.value})}
@@ -93,14 +124,14 @@ const AdminRegistrationModal = ({ isOpen, onRequestClose }) => {
                             type="password"
                             className="form-control"
                             id="confirmPassword"
-                            placeholder="Enter the password again"
+                            placeholder="Enter password again"
                             style={{textAlign: 'center'}}
                             value={formData.confirmPassword}
                             onChange={(e) => setFormData({...formData, confirmPassword: e.target.value})}
                         />
                     </div>
                     <div className="d-flex justify-content-center align-items-center">
-                        <button type="submit" className="btn btn-primary mt-4" disabled={!formData.confirmPassword}>
+                        <button type="submit" className="mt-4 register-admin-button d-flex justify-content-center align-items-center" disabled={!formData.confirmPassword}>
                             Register Admin
                         </button>
                     </div>
