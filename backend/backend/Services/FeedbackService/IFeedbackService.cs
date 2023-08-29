4c2cb0c72269b049d56f1b093fdd89bedeb256dd
lili.farkas <lili.farkas14@gmail.com>
2023-08-29 14:08:52 +0200

    Implement IFeedbackService interface

--- backend/backend/Services/FeedbackService/IFeedbackService.cs
+++ backend/backend/Services/FeedbackService/IFeedbackService.cs
@@ -1,6 +1,11 @@
+using backend.Models.Entities;
+
 namespace backend.Services.FeedbackService;
 
 public interface IFeedbackService
 {
-    
+    public Task<Feedback> Add(Feedback feedback);
+    public Task<IEnumerable<Feedback>> GetAll();
+    public Task Delete(long id);
+    public Task<IEnumerable<Feedback>> GetUserFeedback(long id);
 }
