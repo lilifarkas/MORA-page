d47d5243cae91d142ac5e47117958969e54666c1
lili.farkas <lili.farkas14@gmail.com>
2023-08-30 18:20:27 +0200

    Implement the Feedbacks component

--- frontend/src/components/Feedbacks/Feedbacks.jsx
+++ frontend/src/components/Feedbacks/Feedbacks.jsx
@@ -1,15 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import InfiniteCarousel from './Slider';
+import URL from "../../Constants/ConstantUrl";
 import './Feedbacks.css'
-import bgImg from "../../images/Névtelen terv (23).png";
 
-function Feedbacks(){
+function Feedbacks() {
+
+    const [feedbacks, setFeedbacks] = useState([]);
+
+    useEffect(() => {
+        async function getFeedbacks() {
+            try {
+                const response = await fetch(`${URL}feedbacks`);
+
+                if (!response.ok) {
+                    const message = `An error occurred: ${response.statusText}`;
+                    window.alert(message);
+                    return;
+                }
+
+                const records = await response.json();
+                const formattedFeedbacks = records.map(feedback => ({
+                    css: 'url(./one.svg)',
+                    message: feedback.comment
+                }));
+                setFeedbacks(formattedFeedbacks);
+                console.log(records);
+            } catch (error) {
+                console.error("Error fetching feedbacks:", error);
+            }
+        }
+
+        getFeedbacks();
+    }, []);
     
-
     return (
-        <div className="">
-            HELLO
+        <div>
+            <h3 className="mb-5 feedbacks-title">Feedbacks from our patients...</h3>
+            <div className="main-container-feedbacks">
+                <InfiniteCarousel items={feedbacks} itemWidth={'full'} visible={1}>
+                    {({ message }, i) => (
+                        <div className="content">
+                            <div className="message-text" dangerouslySetInnerHTML={{ __html: `&ldquo;${message}&rdquo;` }}  />
+                        </div>
+                    )}
+                </InfiniteCarousel>
+            </div>
         </div>
     );
 }
-
 
 export default Feedbacks;
