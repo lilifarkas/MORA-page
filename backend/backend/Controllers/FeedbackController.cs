6258f6c75d29756de6e13d63e32c01a148e27dc3
lili.farkas <lili.farkas14@gmail.com>
2023-08-30 10:22:01 +0200

    Implement the FeedbackController

---
+++ backend/backend/Controllers/FeedbackController.cs
@@ -0,0 +1,59 @@
+using backend.Models.Entities;
+using backend.Models.Requests;
+using backend.Services.FeedbackService;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers;
+
+[ApiController, Route("/feedbacks")]
+
+public class FeedbackController: ControllerBase
+{
+    private readonly IFeedbackService _service;
+   
+    public FeedbackController(IFeedbackService service)
+    {
+        _service = service;
+    }
+    
+    [HttpGet("{userID}/feedback")]
+    public async Task<IEnumerable<Feedback>> GetUserFeedback(long userID)
+    {
+        return await _service.GetUserFeedback(userID);
+
+    }
+    
+    [HttpGet]
+    public async Task<IEnumerable<Feedback>> GetAllFeedbacks()
+    {
+        return await _service.GetAll();
+    }
+
+    [HttpPost("registerFeedback")]
+    public async Task<IActionResult> Register([FromBody] RegisterFeedbackRequest registerFeedbackRequest)
+    {
+        var registrationFeedback = new Feedback()
+        {
+            UserId = registerFeedbackRequest.UserId,
+            Date = registerFeedbackRequest.Date,
+            Comment = registerFeedbackRequest.Comment,
+            Rating = registerFeedbackRequest.Rating,
+            IsApproved = registerFeedbackRequest.IsApproved
+        };
+        var response = await _service.Add(registrationFeedback);
+        return Ok(response);
+    }
+    
+    [HttpDelete("/feedbacks/delete/{id}")]
+    public async Task DeleteFeedback(long id)
+    {
+        await _service.Delete(id);
+    }
+    
+    [HttpPut("/update/{id}")]
+    public async Task<IActionResult> UpdateFeedback(long id, [FromBody] EditFeedbackRequest editFeedback)
+    {
+        await _service.UpdateFeedback(id, editFeedback);
+        return Ok();
+    }
+}
