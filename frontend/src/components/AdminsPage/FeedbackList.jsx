a3bd9736e5cb874862e4a882705dbc01bf8a945c
lili.farkas <lili.farkas14@gmail.com>
2023-08-31 20:03:09 +0200

    Add toggleApproval function to change the approval

--- frontend/src/components/AdminsPage/FeedbackList.jsx
+++ frontend/src/components/AdminsPage/FeedbackList.jsx
@@ -4,26 +4,55 @@ import {NavLink, useNavigate} from "react-router-dom";
 import bgImg from "../../images/Névtelen terv (25).png";
 import './AdminsPage.css'
 import {FiArrowLeft} from "react-icons/fi";
+import './FeedbackList.css'
 
-const Record = (props) => (
-    <tr>
-        <td>{props.record.userId}</td>
-        <td>{props.record.date}</td>
-        <td>{props.record.rating}</td>
-        <td>{props.record.comment}</td>
-        <td>
-            {props.record.bookedDates.map((appointments) => {
-                const bookedTime = appointments.bookedTime;
-                const date = new Date(bookedTime);
-                const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1).toString().padStart(2, '0')}-${date.getDate().toString().padStart(2, '0')} : ${date.getHours()}:${date.getMinutes().toString().padStart(2, '0')}`;
+const Record = (props) => {
+    const [isApproved, setIsApproved] = useState(props.record.isApproved);
 
-                return (
-                    <div key={appointments.id}>{formattedDate}</div>
-                );
-            })}
-        </td>
-    </tr>
-);
+    const toggleApproval = async () => {
+        try {
+            const response = await fetch(`${URL}update/${props.record.id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    isApproved: !isApproved, 
+                }),
+            });
+
+            if (response.ok) {
+                setIsApproved(!isApproved);
+                window.location.reload();
+            } else {
+                const errorMessage = await response.text();
+                console.error(errorMessage);
+            }
+        } catch (error) {
+            console.error("Error updating approval:", error);
+        }
+    };
+
+    return (
+        <tr>
+            <td>{props.record.userId}</td>
+            <td>{props.record.date}</td>
+            <td>{props.record.rating}</td>
+            <td style={{width: props.feedbackWidth}}>{props.record.comment}</td>
+            <td>{props.record.isApproved.toString()}</td>
+            <td>
+                <label className="approval-toggle">
+                    <input
+                        type="checkbox"
+                        checked={isApproved}
+                        onChange={toggleApproval}
+                    />
+                    Approved
+                </label>
+            </td>
+        </tr>
+    )
+};
 
 export default function FeedbackList() {
     const [isLoading, setIsLoading] = useState(true);
@@ -61,6 +90,7 @@ export default function FeedbackList() {
                 <Record
                     record={record}
                     key={record._id}
+                    feedbackWidth="70%"
                 />
             );
         });
@@ -121,7 +151,7 @@ export default function FeedbackList() {
                         <div className="">
                             <div className="d-flex flex-row">
                                 <NavLink
-                                    to="/"
+                                    to="/adminsPage"
                                     className="back"
                                 >
                                     <button className="logout">
@@ -129,7 +159,7 @@ export default function FeedbackList() {
                                     </button>
                                 </NavLink>
                             </div>
-                            <div className="title-container">
+                            <div className="title-container-feedback">
                                 <h3 className="title-users">Feedbacks</h3>
                             </div>
                             <div className="filter">
@@ -147,15 +177,16 @@ export default function FeedbackList() {
                                 </div>
                             </div>
                         </div>
-                        <div className="table-container">
+                        <div className="table-container-feedback">
                             <table className="table" style={{ marginTop: 40 }}>
                                 <thead>
                                 <tr>
                                     <th>User ID</th>
                                     <th>Date</th>
                                     <th>Rating</th>
-                                    <th>Feedback</th>
-                                    <th>Approval</th>
+                                    <th style={{ width: '70%' }}>Feedback</th>
+                                    <th>Is approved</th>
+                                    <th>Change approval</th>
                                 </tr>
                                 </thead>
                                 <tbody>{recordList()}</tbody>
